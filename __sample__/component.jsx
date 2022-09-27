ef6cd2b75e5d827987d740bad7416772f90a1e1f
Owen Adams <owenadams.main@gmail.com>
2022-09-27 00:30:34 +0000

    null

--- __sample__/component.jsx
+++ __sample__/component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Sample extends React.Component {
+export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
